4e4897ad30c972cdff1d3e90c011b490bd3d6905
Namdev t s <namdevts123@gmail.com>
2025-03-11 21:59:40 -0700

    CaseString.java

---
+++ CaseString.java
@@ -0,0 +1,9 @@
+public class CaseString {
+    public static void main(String[] args) {
+        
+        String s1 = "Good Morning";
+        System.out.println(s1);
+        System.err.println("To Uppercase:"+s1.toUpperCase());
+        System.err.println("To lowercase:"+s1.toLowerCase());
+    }
+}
